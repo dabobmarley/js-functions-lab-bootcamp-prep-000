55c100b8446ff856e6e381fc0d8ecf42732eda56
dabobmarley@hotmail.com <dabobmarley@hotmail.com>
2018-05-16 15:16:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ return "Happy holidays!"
 }
 
 function happyHolidaysTo(name){
-  console.log'Happy holidays, ${name}!'
+  console.log('Happy holidays, ${name}!')
 }
 
 happyHolidaysTo("you")
 
 function happyHolidayTo(holiday, name){
-  return "Happy ${holiday}, ${name}!"
+  return ("Happy ${holiday}, ${name}!")
 }
 
 happyHolidayTo("Independence Day", "you")
